bfd72cee2e00fc17391512b314eff8bb59ce5884
Marc416 <juni416@naver.com>
2025-04-16 14:49:48 +0900

    feat : controller_exception_handler 순환참조 제거

--- src/rbac/main.py
+++ src/rbac/main.py
@@ -3,6 +3,7 @@ import logging
 from fastapi import FastAPI
 
 from rbac.application.account.controller import account_command_controller
+from rbac.application.exception.controller_exception_handler import register_exception_handlers
 from rbac.application.middleware.request_context_middleware import RequestContextMiddleware
 from rbac.application.project.controller import project_command_controller, project_query_controller
 from rbac.utils.life import lifespan
@@ -14,9 +15,8 @@ logging.basicConfig(
 
 app = FastAPI(lifespan=lifespan)
 app.add_middleware(RequestContextMiddleware)
-# TODO app.handler가 등록이 안돼서 임의적으로 실행시키려고 임포트하였는데 다른 방법을 찾아봐야 할거 같음.
-import rbac.application.exception.controller_exception_handler as exception_handler
-exception_handler
+register_exception_handlers(app)
+
 app.include_router(account_command_controller.router)
 app.include_router(project_command_controller.router)
 
